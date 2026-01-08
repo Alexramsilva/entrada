61ef5067357257d8727f783c2c07ee55014dcd9b
UNRC_Hub <106453707+Alexramsilva@users.noreply.github.com>
2026-01-08 13:03:47 -0600

    entrada.py

--- entrada.py
+++ entrada.py
@@ -168,7 +168,6 @@ st.subheader("Ranking de proximidad al Entry")
 st.dataframe(
     df_screen[[
         "Ranking",
-        "Estado",
         "Ticker",
         "Precio actual",
         "|x| actual",
