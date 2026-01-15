b6eab0fd143d5e424b8e2244b89e5f15d6f41e93
UNRC_Hub <106453707+Alexramsilva@users.noreply.github.com>
2026-01-15 10:20:17 -0600

    entrada.py

--- entrada.py
+++ entrada.py
@@ -25,7 +25,7 @@ TICKERS = [
     "BTC-USD", "GAPB.MX", "PLTR", "SPY", "GRUMAB.MX",
     "FMTY14.MX", "IAU", "HOOD", "SOFI", "NVDA",
     "WALMEX.MX", "FUNO11.MX", "CHDRAUIB.MX",
-    "QUBT", "FRES.MX", "JPM", "MCHI", "INDA", "GMEXICOB.MX", "TSM", "AMD", "GOOGL", "IBM", "AMZN", "AVGO", "V"
+    "QUBT", "FRES.MX", "JPM", "MCHI", "INDA", "GMEXICOB.MX", "TSM", "AMD", "GOOGL", "IBM", "AMZN", "AVGO", "V", "ASURB.MX"
 ]
 
 st.image("shop.png", width=300)
